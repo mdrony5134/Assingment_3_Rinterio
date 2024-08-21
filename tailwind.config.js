651b3ff29421a44e7b1fc991a0b2cae8f2397abe
mdrony5134 <rony830388@gmail.com>
2024-08-21 23:33:04 +0600

    nav and banner section done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+      colors:{
+        'primary-color':"#212121",
+        'secondary-color': "#494949",
+        'third-color':'#737373',
+        // 'background-color':'#ABEF5F',
+      },
+      backgroundColor:{
+        'primary':'#ABEF5F',
+      }
+    },
+    plugins: [],
+  }
